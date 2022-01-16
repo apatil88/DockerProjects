5762f0aaf6a114f1054460d04786f60405195419
Amrut Patil <amrutp6491+traininggeneral@gmail.com>
2022-01-15 20:13:58 -0500

    Initial app setup

---
+++ DockerComposeDemo/index.js
@@ -0,0 +1,23 @@
+//Simple NodeJS app to demonstrate use of Docker Compose
+//NodeJS app resides in a separate container
+//Redis resides in a separate container
+
+//NodeJS app uses redis client to get count stored in redis cache and display on the front end
+
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient();
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+  });
+});
+
+app.listen(8081, () => {
+  console.log('Listening on port 8081');
+});
